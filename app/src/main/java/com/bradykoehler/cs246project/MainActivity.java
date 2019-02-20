ce8af3efb9b7076866159a4d5663ca01cb0c941e
Brady Koehler <me@bradykoehler.com>
2019-02-20 13:41:07 -0700

    Update comment

--- app/src/main/java/com/bradykoehler/cs246project/MainActivity.java
+++ app/src/main/java/com/bradykoehler/cs246project/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // go away kendra
+        // go away kendra now
     }
 }
