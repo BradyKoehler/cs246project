e6734c24b8467b0aa7f3c3b83214ede24644e79f
caitycatt <pro17001@byui.edu>
2019-02-25 13:01:32 -0700

    deleted irrelavent stuff, like comments

--- app/src/main/java/com/bradykoehler/cs246project/MainActivity.java
+++ app/src/main/java/com/bradykoehler/cs246project/MainActivity.java
@@ -9,8 +9,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        // go away kendra, youre taking forever hi
-        // at least i like you kendra
     }
 }
