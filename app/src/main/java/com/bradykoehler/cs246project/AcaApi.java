1be36ab9d39dc1bfb65f9a35c0576383dd22de7f
Brady Koehler <me@bradykoehler.com>
2019-04-04 17:40:09 -0600

    Clean up AcaApi.java

--- app/src/main/java/com/bradykoehler/cs246project/AcaApi.java
+++ app/src/main/java/com/bradykoehler/cs246project/AcaApi.java
@@ -15,16 +15,14 @@ import java.lang.ref.WeakReference;
 import java.net.URL;
 import java.util.Scanner;
 
-public class AcaApi {
+class AcaApi {
     private static final AcaApi ourInstance = new AcaApi();
 
-    public static AcaApi getInstance() {
+    static AcaApi getInstance() {
         return ourInstance;
     }
 
-    public static Image img;
-
-    public static String baseUrl = "https://ifd58d1qri.execute-api.us-east-1.amazonaws.com/latest";
+    private static String baseUrl = "https://ifd58d1qri.execute-api.us-east-1.amazonaws.com/latest";
 
     private AcaApi() {
 
@@ -32,11 +30,10 @@ public class AcaApi {
 
     private String getAccessToken(AppCompatActivity activity) {
         SharedPreferences loginData = activity.getSharedPreferences("jwt", Context.MODE_PRIVATE);
-        String token = loginData.getString("accessToken", null);
-        return token;
+        return loginData.getString("accessToken", null);
     }
 
-    public void getGrids(final NavMainActivity activity) {
+    void getGrids(final NavMainActivity activity) {
         final WeakReference<NavMainActivity> main = new WeakReference<>(activity);
 
         Log.d("AcaApi", "Running getGrids()");
@@ -44,39 +41,39 @@ public class AcaApi {
             @RequiresApi(api = Build.VERSION_CODES.KITKAT)
             @Override
             public void run() {
-                try {
-                    Log.d("AcaApi", "Trying to open URL");
+            try {
+                Log.d("AcaApi", "Trying to open URL");
 
-                    // build request url
-                    InputStream response = new URL(baseUrl + "/grids?token=" + getAccessToken(activity)).openStream();
+                // build request url
+                InputStream response = new URL(baseUrl + "/grids?token=" + getAccessToken(activity)).openStream();
 
-                    // check for response
-                    try (Scanner scanner = new Scanner(response)) {
-                        // read json
-                        String responseBody = scanner.useDelimiter("\\A").next();
+                // check for response
+                try (Scanner scanner = new Scanner(response)) {
+                    // read json
+                    String responseBody = scanner.useDelimiter("\\A").next();
 
-                        Log.d("AcaApi", responseBody);
+                    Log.d("AcaApi", responseBody);
 
-                        final Grid[] grids = new Gson().fromJson(responseBody, Grid[].class);
+                    final Grid[] grids = new Gson().fromJson(responseBody, Grid[].class);
 
-                        Log.d("AcaApi", "Retrieved Grids list");
+                    Log.d("AcaApi", "Retrieved Grids list");
 
-                        main.get().runOnUiThread(new Runnable() {
-                            @Override
-                            public void run() {
-                                Log.d("AcaApi", "Attempting to load new grids");
-                                main.get().loadGrids(grids);
-                            }
-                        });
-                    }
-                } catch (IOException e) {
-                    e.printStackTrace();
+                    main.get().runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                        Log.d("AcaApi", "Attempting to load new grids");
+                        main.get().loadGrids(grids);
+                        }
+                    });
                 }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
             }
         }).start();
     }
 
-    public void createGrid(final NavMainActivity activity, final String name) {
+    void createGrid(final NavMainActivity activity, final String name) {
         final WeakReference<NavMainActivity> main = new WeakReference<>(activity);
 
         Log.d("AcaApi", "Running createGrid()");
@@ -84,128 +81,37 @@ public class AcaApi {
             @RequiresApi(api = Build.VERSION_CODES.KITKAT)
             @Override
             public void run() {
-//                OutputStream out = null;
-                try {
+            try {
+                // build request url
+                InputStream response = new URL(baseUrl + "/grids/create/" + name + "?token=" + getAccessToken(activity)).openStream();
 
+                // check for response
+                try (Scanner scanner = new Scanner(response)) {
+                    // read json
+                    String responseBody = scanner.useDelimiter("\\A").next();
 
-//                    JSONObject jsonParam = new JSONObject();
-//                    jsonParam.put("name", name);
-//                    String dataString = jsonParam.toString();
-//
-//                    URL url = new URL(baseUrl + "/grids/" + name);
-//                    HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
-//                    urlConnection.setRequestMethod("POST");
-//                    out = new BufferedOutputStream(urlConnection.getOutputStream());
-//
-//                    BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(out, "UTF-8"));
-//                    writer.write(dataString);
-//                    writer.flush();
-//                    writer.close();
-//                    out.close();
-//
-//                    urlConnection.connect();
-////
-//                    Log.i("STATUS", String.valueOf(urlConnection.getResponseCode()));
-//                    Log.i("MSG" , urlConnection.getResponseMessage());
-//                    Log.i("BODY", urlConnection.getContent().toString());
-//
-//                    urlConnection.disconnect();
+                    Log.d("AcaApi", responseBody);
 
-//
-//                    URL url = new URL(baseUrl + "/grids");
-//                    HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-//                    conn.setRequestMethod("POST");
-//                    conn.setRequestProperty("Content-Type", "application/json;charset=UTF-8");
-//                    conn.setRequestProperty("Accept","application/json");
-//                    conn.setDoOutput(true);
-//                    conn.setDoInput(true);
-//
-//                    JSONObject jsonParam = new JSONObject();
-//                    jsonParam.put("name", name);
-////                    jsonParam.put("timestamp", 1488873360);
-////                    jsonParam.put("uname", message.getUser());
-////                    jsonParam.put("message", message.getMessage());
-////                    jsonParam.put("latitude", 0D);
-////                    jsonParam.put("longitude", 0D);
-//
-//                    Log.i("JSON", jsonParam.toString());
-//                    DataOutputStream os = new DataOutputStream(conn.getOutputStream());
-//                    //os.writeBytes(URLEncoder.encode(jsonParam.toString(), "UTF-8"));
-//                    os.writeBytes(jsonParam.toString());
-//
-//                    os.flush();
-//                    os.close();
-//
-//                    Log.i("STATUS", String.valueOf(conn.getResponseCode()));
-//                    Log.i("MSG" , conn.getResponseMessage());
-//
-//                    conn.disconnect();
+                    // final Image[] images = new Gson().fromJson(responseBody, Image[].class);
+                    final Grid grid = new Gson().fromJson(responseBody, Grid.class);
 
-//                     build request url
-                    InputStream response = new URL(baseUrl + "/grids/create/" + name + "?token=" + getAccessToken(activity)).openStream();
+                    Log.d("AcaApi", "Created Grid: " + responseBody);
 
-                    // check for response
-                    try (Scanner scanner = new Scanner(response)) {
-                        // read json
-                        String responseBody = scanner.useDelimiter("\\A").next();
-
-                        Log.d("AcaApi", responseBody);
-
-//                        final Image[] images = new Gson().fromJson(responseBody, Image[].class);
-                        final Grid grid = new Gson().fromJson(responseBody, Grid.class);
-
-                        Log.d("AcaApi", "Created Grid: " + responseBody);
-
-                        main.get().runOnUiThread(new Runnable() {
-                            @Override
-                            public void run() {
-//                                Log.d("AcaApi", "Attempting to load images");
-//                                main.get().getImages(images);
-//                                main.get().loadImages(images);
-                                main.get().addGrid(grid);
-                            }
-                        });
-                    }
-
-//                    main.get().runOnUiThread(new Runnable() {
-//                        @Override
-//                        public void run() {
-//                            main.get().startActivity(new Intent(main.get(), GridActivity.class));
-//                        }
-//                    });
-//                    Log.d("AcaApi", "Trying to open URL");
-//
-//                    // build request url
-//                    InputStream response = new URL(baseUrl + "/images").openStream();
-//
-//                    // check for response
-//                    try (Scanner scanner = new Scanner(response)) {
-//                        // read json
-//                        String responseBody = scanner.useDelimiter("\\A").next();
-//
-//                        Log.d("AcaApi", responseBody);
-//
-//                        final Image[] images = new Gson().fromJson(responseBody, Image[].class);
-//
-//                        Log.d("AcaApi", "Retrieved Images list");
-//
-//                        main.get().runOnUiThread(new Runnable() {
-//                            @Override
-//                            public void run() {
-//                                Log.d("AcaApi", "Attempting to load images");
-////                                main.get().getImages(images);
-//                                main.get().loadImages(images);
-//                            }
-//                        });
-//                    }
-                } catch (Exception e) {
-                    e.printStackTrace();
+                    main.get().runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            main.get().addGrid(grid);
+                        }
+                    });
                 }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
             }
         }).start();
     }
 
-    public void getGrid(final GridActivity activity, final int id) {
+    void getGrid(final GridActivity activity, final int id) {
         final WeakReference<GridActivity> main = new WeakReference<>(activity);
 
         Log.d("AcaApi", "Running getGrid()");
@@ -213,44 +119,41 @@ public class AcaApi {
             @RequiresApi(api = Build.VERSION_CODES.KITKAT)
             @Override
             public void run() {
-                try {
-                    Log.d("AcaApi", "Trying to open URL");
+            try {
+                Log.d("AcaApi", "Trying to open URL");
 
-                    // build request url
-                    InputStream response = new URL(baseUrl + "/grid/" + id + "?token=" + getAccessToken(activity)).openStream();
+                // build request url
+                InputStream response = new URL(baseUrl + "/grid/" + id + "?token=" + getAccessToken(activity)).openStream();
 
-                    // check for response
-                    try (Scanner scanner = new Scanner(response)) {
-                        // read json
-                        String responseBody = scanner.useDelimiter("\\A").next();
+                // check for response
+                try (Scanner scanner = new Scanner(response)) {
+                    // read json
+                    String responseBody = scanner.useDelimiter("\\A").next();
 
-                        Log.d("AcaApi", responseBody);
+                    Log.d("AcaApi", responseBody);
 
-//                        final Grid grid = new Gson().fromJson(responseBody, Grid.class);
-                        final Tile[] tiles = new Gson().fromJson(responseBody, Tile[].class);
-                        Log.d("AcaApi", "Retrieved Grid data");
+                    final Tile[] tiles = new Gson().fromJson(responseBody, Tile[].class);
+                    Log.d("AcaApi", "Retrieved Grid data");
 
-                        main.get().runOnUiThread(new Runnable() {
-                            @Override
-                            public void run() {
-                                Log.d("AcaApi", "Attempting to load tiles for grid");
+                    main.get().runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                        Log.d("AcaApi", "Attempting to load tiles for grid");
 
-                                for (int i = 0; i < tiles.length; i++)
-                                {
-                                    Tile tile = tiles[i];
-                                    main.get().setTile(tile);
-                                }
+                            for (Tile tile : tiles) {
+                                main.get().setTile(tile);
                             }
-                        });
-                    }
-                } catch (IOException e) {
-                    e.printStackTrace();
+                        }
+                    });
                 }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
             }
         }).start();
     }
 
-    public void getImages(final ActivityTileSel activity) {
+    void getImages(final ActivityTileSel activity) {
         final WeakReference<ActivityTileSel> main = new WeakReference<>(activity);
 
         Log.d("AcaApi", "Running getImages()");
@@ -258,40 +161,39 @@ public class AcaApi {
             @RequiresApi(api = Build.VERSION_CODES.KITKAT)
             @Override
             public void run() {
-                try {
-                    Log.d("AcaApi", "Trying to open URL");
+            try {
+                Log.d("AcaApi", "Trying to open URL");
 
-                    // build request url
-                    InputStream response = new URL(baseUrl + "/images?token=" + getAccessToken(activity)).openStream();
+                // build request url
+                InputStream response = new URL(baseUrl + "/images?token=" + getAccessToken(activity)).openStream();
 
-                    // check for response
-                    try (Scanner scanner = new Scanner(response)) {
-                        // read json
-                        String responseBody = scanner.useDelimiter("\\A").next();
+                // check for response
+                try (Scanner scanner = new Scanner(response)) {
+                    // read json
+                    String responseBody = scanner.useDelimiter("\\A").next();
 
-                        Log.d("AcaApi", responseBody);
+                    Log.d("AcaApi", responseBody);
 
-                        final Image[] images = new Gson().fromJson(responseBody, Image[].class);
+                    final Image[] images = new Gson().fromJson(responseBody, Image[].class);
 
-                        Log.d("AcaApi", "Retrieved Images list");
+                    Log.d("AcaApi", "Retrieved Images list");
 
-                        main.get().runOnUiThread(new Runnable() {
-                            @Override
-                            public void run() {
-                                Log.d("AcaApi", "Attempting to load images");
-//                                main.get().getImages(images);
-                                main.get().loadImages(images);
-                            }
-                        });
-                    }
-                } catch (IOException e) {
-                    e.printStackTrace();
+                    main.get().runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                        Log.d("AcaApi", "Attempting to load images");
+                        main.get().loadImages(images);
+                        }
+                    });
                 }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
             }
         }).start();
     }
 
-    public void createTile(final GridActivity activity, final int id, final int pos, final int gridId) {
+    void createTile(final GridActivity activity, final int id, final int pos, final int gridId) {
         final WeakReference<GridActivity> main = new WeakReference<>(activity);
 
         Log.d("AcaApi", "Running createTile()");
@@ -299,33 +201,29 @@ public class AcaApi {
             @RequiresApi(api = Build.VERSION_CODES.KITKAT)
             @Override
             public void run() {
-                try {
-                    Log.d("AcaApi", "Trying to open URL");
+            try {
+                Log.d("AcaApi", "Trying to open URL");
 
-                    // build request url
-                    InputStream response = new URL(baseUrl + "/grid/" + gridId + "/tiles/new/" + id + "/" + pos + "?token=" + getAccessToken(activity)).openStream();
+                // build request url
+                InputStream response = new URL(baseUrl + "/grid/" + gridId + "/tiles/new/" + id + "/" + pos + "?token=" + getAccessToken(activity)).openStream();
 
-                    // check for response
-                    try (Scanner scanner = new Scanner(response)) {
-                        // read json
-                        String responseBody = scanner.useDelimiter("\\A").next();
+                // check for response
+                try (Scanner scanner = new Scanner(response)) {
+                    // read json
+                    String responseBody = scanner.useDelimiter("\\A").next();
 
-                        Log.d("AcaApi:createTile", responseBody);
+                    Log.d("AcaApi:createTile", responseBody);
 
-//                        final Grid grid = new Gson().fromJson(responseBody, Grid.class);
-//                        final Tile[] tiles = new Gson().fromJson(responseBody, Tile[].class);
-
-                        main.get().runOnUiThread(new Runnable() {
-                            @Override
-                            public void run() {
-//                                Log.d("AcaApi", "Attempting to load tiles for grid");
-                                // Do something with the tiles here
-                            }
-                        });
-                    }
-                } catch (IOException e) {
-                    e.printStackTrace();
+                    main.get().runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                        // Do something with the tiles here
+                        }
+                    });
                 }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
             }
         }).start();
     }
